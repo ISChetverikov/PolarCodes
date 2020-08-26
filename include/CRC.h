d8f3348a615fbb08d7a1ae7b1b4ddec987d2e993
Ilya <ischetverikov@gmail.com>
2020-08-26 14:41:37 +0300

    CRC

--- include/CRC.h
+++ include/CRC.h
@@ -5,9 +5,16 @@
 class CRC {
 private:
 	std::vector<int> _poly;
+	std::vector<int> _init;
+
+	size_t _deg = 0;
+	int _paddingSymbol = 0;
 
 public:
+
 	CRC(std::vector<int> poly);
-	std::vector<int> GetSum(std::vector<int> bits);
-	bool Check(std::vector<int> bytes, std::vector<int> crc);
+	std::vector<int> Calculate(std::vector<int> bits);
+
+	std::vector<int> Add(std::vector<int> bits);
+	bool Check(std::vector<int> bits);
 };
