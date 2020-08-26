74230d8cd03acc6e598eb718c6b39b266ddf2c4f
Ilya <ischetverikov@gmail.com>
2020-08-26 17:11:48 +0300

    Add, Check

--- src/crcTest.cpp
+++ src/crcTest.cpp
@@ -5,15 +5,18 @@
 int main() {
 	std::vector<int> poly = { 1, 1, 1, 0, 0, 0, 0, 0, 1};
 	//std::vector<int> input = { 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-	std::vector<int> input = { 1, 1, 1, 0};
+	std::vector<int> input = { 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0 };
 
 	auto c = CRC(poly);
-	std::vector<int> hash = c.Calculate(input);
-	for (size_t i = 0; i < hash.size(); i++)
+	std::vector<int> hash = c.Add(input);
+	int length = hash.size();
+	for (int i = length - 1; i >= 0; i--)
 	{
 		std::cout << hash[i];
 	}
 	std::cout << std::endl;
+	bool ch = c.Check(hash);
+	std::cout << ch << std::endl;
 
 	return 0;
 }
