5dbd882acfbf17312950ae445b6a9dce556b760e
Ilya <ischetverikov@gmail.com>
2020-09-13 19:48:20 +0300

    Stat decoder

--- include/DecoderType.h
+++ include/DecoderType.h
@@ -3,7 +3,7 @@
 #include <string>
 #include <unordered_map>
 
-enum decoderType { SC, SCRecursive, SCFano, UnknownDecoder, SCFlip, SCFlipProg, SCList };
+enum decoderType { SC, SCRecursive, SCFano, UnknownDecoder, SCFlip, SCFlipProg, SCList, SCFlipStat };
 
 decoderType decoderTypeFromString(std::string str);
 std::string decoderTypeToString(decoderType type);
