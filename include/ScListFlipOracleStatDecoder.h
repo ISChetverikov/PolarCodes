f7da688e53bf61a2a71313c12e0040b09365aedf
Ilya <ischetverikov@gmail.com>
2020-10-16 17:59:07 +0300

    Oracle decoder save state

--- include/ScListFlipOracleStatDecoder.h
+++ include/ScListFlipOracleStatDecoder.h
@@ -8,9 +8,16 @@ private:
 	std::map<int, int> _singleOracleFlipsStat;
 	std::map<std::tuple<int, int>, int> _doubleOracleFlipsStat;
 	std::map<std::tuple<int, int, int>, int> _tripleOracleFlipsStat;
+
+	int _count = 0;
+	int _countErrorneous = 0;
+	int _singleSuccessfulFlips = 0;
+	int _doubleSuccessfulFlips = 0;
+	int _tripleSuccessfulFlips = 0;
 	
 protected:
-	int GetFirstErrorPosition(std::vector<int> codeword1, std::vector<int> codeword2);
+	int GetFirstErrorPositionMetric(std::vector<std::vector<int>> candidates, std::vector<double> metrics, std::vector<int> originalCodeword);
+	int GetFirstErrorPositionMaxDistance(std::vector<std::vector<int>> candidates, std::vector<int> originalCodeword);
 
 public:
 	ScListFlipOracleStatDecoder(PolarCode * code, int L);
--- src/ScListFlipOracleStatDecoder.cpp
+++ src/ScListFlipOracleStatDecoder.cpp
@@ -1,4 +1,5 @@
 
+#include <algorithm>
 #include <sstream>
 #include "../include/ScListFlipOracleStatDecoder.h"
 
@@ -7,28 +8,57 @@
 ScListFlipOracleStatDecoder::ScListFlipOracleStatDecoder(PolarCode * codePtr, int L) : ScListFlipStatDecoder(codePtr, L) {
 }
 
-int ScListFlipOracleStatDecoder::GetFirstErrorPosition(std::vector<int> codeword1, std::vector<int> codeword2) {
+int ScListFlipOracleStatDecoder::GetFirstErrorPositionMetric(std::vector<std::vector<int>> candidates, std::vector<double> metrics, std::vector<int> originalCodeword) {
+	
+	auto maxIt = std::max_element(metrics.begin(), metrics.end());
+	int maxInd = (int)std::distance(metrics.begin(), maxIt);
+	
 	int i = 0;
-
-	for (; i < codeword1.size(); i++)
+	for (; i < originalCodeword.size(); i++)
 	{
-		if (codeword1[i] != codeword2[i])
+		if (candidates[maxInd][i] != originalCodeword[i])
 			break;
 	}
 
 	return i;
 }
 
+int ScListFlipOracleStatDecoder::GetFirstErrorPositionMaxDistance(std::vector<std::vector<int>> candidates, std::vector<int> originalCodeword) {
+
+	int n = (int)originalCodeword.size();
+
+	int best = 0;
+	for (int i = 0; i < _L; i++)
+	{
+		int j = 0;
+		for (; j < n; j++)
+		{
+			if (candidates[i][j] != originalCodeword[j])
+				break;
+		}
+		
+		if (j > best)
+			best = j;
+	}
+
+	return best;
+}
+
 void ScListFlipOracleStatDecoder::ClearStatistic() {
 	_singleOracleFlipsStat.clear();
 	_doubleOracleFlipsStat.clear();
 	_tripleOracleFlipsStat.clear();
+
+	_countErrorneous = 0;
+	_singleSuccessfulFlips = 0;
+	_doubleSuccessfulFlips = 0;
+	_tripleSuccessfulFlips = 0;
 }
 
 std::string ScListFlipOracleStatDecoder::GetStatistic() {
 	std::stringstream ss;
-	// std::sort(_singleFlipStatistic.rbegin(), _singleFlipStatistic.rend());
-	ss << "Single Flip:\n";
+	
+	/*ss << "Single Flip:\n";
 	for (std::pair<int, int> single : _singleOracleFlipsStat)
 	{
 		ss << "(" << single.first << "): " << single.second << "\n";
@@ -44,35 +74,48 @@ std::string ScListFlipOracleStatDecoder::GetStatistic() {
 	for (std::pair<std::tuple<int, int, int>, int> triple : _tripleOracleFlipsStat)
 	{
 		ss << "(" << std::get<0>(triple.first) << ", " << std::get<1>(triple.first) << ", " << std::get<2>(triple.first) << "): " << triple.second << "\n";
-	}
+	}*/
 
+	ss << "Count:" << _count << "\n";
+	ss << "List Error Count: " << _countErrorneous << "\n";
+	ss << "Single: " << _singleSuccessfulFlips << "\n";
+	ss << "Double: " << _doubleSuccessfulFlips << "\n";
+	ss << "Triple: " << _tripleSuccessfulFlips << "\n";
+	
 	return ss.str();
 }
-
-
 
 std::vector<int>  ScListFlipOracleStatDecoder::Decode(std::vector<double> beliefs) {
 	std::vector<int> actualCodeword(beliefs.size(), 0);
 	std::vector<int> flipPositions;
 	bool isError = false;
 	
+	_count++;
+
 	DecodeListInternal(beliefs);
-		
+	std::vector<double> metrics = _metrics; // Take result changes the metrics
 	std::vector<int> result = TakeListStatResult(actualCodeword);
 	isError = actualCodeword != _codeword;
 
 	if (!isError)
 		return result;
 
-	int firstErrorInd = GetFirstErrorPosition(actualCodeword, _codeword);
+	_countErrorneous++;
+
+	// Single
+	int firstErrorInd = GetFirstErrorPositionMaxDistance(_candidates, _codeword);
 	flipPositions.clear();
 	flipPositions.push_back(firstErrorInd);
 
 	DecodeFlipListInternal(beliefs, flipPositions);
+
+	metrics = _metrics;
+
 	result = TakeListStatResult(actualCodeword);
 	isError = actualCodeword != _codeword;
 
 	if (!isError) {
+		_singleSuccessfulFlips++;
 		if (_singleOracleFlipsStat.find(firstErrorInd) == _singleOracleFlipsStat.end())
 			_singleOracleFlipsStat[firstErrorInd] = 1;
 		else
@@ -81,14 +124,20 @@ std::vector<int>  ScListFlipOracleStatDecoder::Decode(std::vector<double> belief
 		return result;
 	}
 	
-	int secondErrorInd = GetFirstErrorPosition(actualCodeword, _codeword);
+
+	// Doulbe
+	int secondErrorInd = GetFirstErrorPositionMaxDistance(_candidates, _codeword);
 	flipPositions.push_back(secondErrorInd);
 
 	DecodeFlipListInternal(beliefs, flipPositions);
+
+	metrics = _metrics;
+
 	result = TakeListStatResult(actualCodeword);
 	isError = actualCodeword != _codeword;
 
 	if (!isError) {
+		_doubleSuccessfulFlips++;
 		auto tuple = std::make_tuple(firstErrorInd, secondErrorInd);
 
 		if (_doubleOracleFlipsStat.find(tuple) == _doubleOracleFlipsStat.end())
@@ -99,7 +148,8 @@ std::vector<int>  ScListFlipOracleStatDecoder::Decode(std::vector<double> belief
 		return result;
 	}
 	
-	int thirdErrorInd = GetFirstErrorPosition(actualCodeword, _codeword);
+	// Triple
+	int thirdErrorInd = GetFirstErrorPositionMaxDistance(_candidates, _codeword);
 	flipPositions.push_back(thirdErrorInd);
 
 	DecodeFlipListInternal(beliefs, flipPositions);
@@ -107,6 +157,7 @@ std::vector<int>  ScListFlipOracleStatDecoder::Decode(std::vector<double> belief
 	isError = actualCodeword != _codeword;
 
 	if (!isError) {
+		_tripleSuccessfulFlips++;
 		auto tuple = std::make_tuple(firstErrorInd, secondErrorInd, thirdErrorInd);
 
 		if (_tripleOracleFlipsStat.find(tuple) == _tripleOracleFlipsStat.end())
